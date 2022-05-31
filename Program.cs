593f2d191e8fbedf691180d1b6172b59771d4556
Thiago César <limongreen20@gmail.com>
2022-05-31 16:54:48 -0300

    operação subtrair

--- Program.cs
+++ Program.cs
@@ -4,5 +4,10 @@ var n2 = Convert.ToDouble(args[2]);
 
 if(operacao == "Somar")
 {
- Console.WriteLine(n1+n2);
+    Console.WriteLine(n1+n2);
+}
+
+if (operacao == "Subtrair" )
+{
+    Console.WriteLine(n1-n2);
 }
